7dd0d39ed29dbf0d142585504ee435e780dbfa94
HieuTong <hieuminhtongle20111998@gmail.com>
2020-05-26 20:26:26 +0700

    Login csv-data driven

---
+++ Test Suites/Run Masttermost Tests.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Run Masttermost Tests</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>7273f097-4f3c-4484-b529-8634f2d4e32d</testSuiteGuid>
+   <testCaseLink>
+      <guid>5f75e1d9-65c1-4d77-9bf6-153daa643c89</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login user accounts</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>1dd1038f-f697-44bd-9184-62a773bc314a</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/User Accounts</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>1dd1038f-f697-44bd-9184-62a773bc314a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>5c713999-ae3b-47b6-b11b-91edfe537765</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>1dd1038f-f697-44bd-9184-62a773bc314a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>2110bb5a-a635-4474-960b-85e6b8bc644f</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
